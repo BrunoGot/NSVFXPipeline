b3564acd639bcf28bbc763090ad76025d1f810c2
DESKTOP-E60HUU4\Natspir <Bruno.got@orange.fr>
2023-10-07 10:37:22 +0200

    clean few line of code of bkender_shelf, commit before changes on Save GUI

--- pipeline/tools/engine/blender/blender_render.py
+++ pipeline/tools/engine/blender/blender_render.py
@@ -8,4 +8,5 @@ def update_render_path():
     """update the file path for render"""
     render_path = os.path.join(fs.asset_base_path,fs.get_render_path(bpy.data.filepath))
     os.makedirs(render_path, exist_ok=True)
+    print(f"render âth = {render_path}")
     bpy.context.scene.render.filepath = render_path+"_"
--- pipeline/tools/engine/blender/blender_shelf.py
+++ pipeline/tools/engine/blender/blender_shelf.py
@@ -5,13 +5,10 @@ import bpy
 from pipeline import fileSystem as fs
 from pipeline.tools.engine import engine
 from pipeline.tools.engine.blender import blender_engine, blender_save_ui
-from pipeline.tools.engine.blender.blender_save_ui import BlenderSaveUI
 
 def save_asset(path):
     pass
-    print("ttessst")
-    """window_ui = blender_save_ui.BlenderSaveUI(blender_engine)
-    window_ui.show()"""
+    print("blender_shelf.save_asset")
     blender_save_ui.show_ui(blender_engine)
 
 
